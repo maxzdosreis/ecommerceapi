100037f609127a8c2ea01ec60a63b320f1c31786
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2025-10-17 21:22:06 -0300

    FIX(exception): alteração no parâmetro da exception
    retira o parâmetro, pois já retorna uma mensagem padrão na exception

--- src/main/java/org/maxzdosreis/ecommerceapi/exception/RequiredObjectIsNullException.java
+++ src/main/java/org/maxzdosreis/ecommerceapi/exception/RequiredObjectIsNullException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class RequiredObjectIsNullException extends RuntimeException {
 
-    public RequiredObjectIsNullException(String message) {
+    public RequiredObjectIsNullException() {
         super("It is not allowed to persist a null object");
     }
 }
